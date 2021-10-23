b6dabdd397609f9e13d379275ac545b8c4050f5c
Patrícia Bandeira <patybandeira.26@gmail.com>
2021-10-23 02:02:02 -0300

    Arrumando erros

--- dados.php
+++ dados.php
@@ -1,25 +1,18 @@
 <?php
-
-echo "<h1>Dados cadastrados</h1>";
+echo "<body style='background-color:pink'>";
+echo "<center><h1>Dados cadastrados</h1></center>";
 echo "<hr>";
-echo "Nome:  ";echo $_POST['nome'];
-echo "<br> <br>";
+echo "<h1>Nome:<h1>  ";echo $_POST['nome'];
 echo "E-mail:  ";echo $_POST['email'];
 echo "<br> <br>";
 echo "Celular:  "; echo $_POST['Celular'];
 echo "<br> <br>";
-echo "Data de Nascimento:  "; echo $_POST['dia']; echo "/"; echo $_POST['mes']; echo "/";echo $_POST['ano'];
-echo "<br><br>";
-
-$dia = $_POST['dia'];
-$mes = $_POST['mes'];
-$ano = $_POST['ano'];
-
+echo "Data de Nascimento:  "; echo $_POST['data'];
+echo "<br> <br>";
+echo "<hr>";
+$datadonascimento=$_POST['data'];
+list($ano, $mes, $dia) = explode('-', $datadonascimento);
 $hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
 $diadonascimento = mktime( 0, 0, 0, $mes, $dia, $ano);
 $idade = floor((((($hoje - $diadonascimento) / 60) / 60) / 24) / 365.25);
-
-echo "<hr>";
 echo 'Você tem '. $idade . ' anos!!';
-echo "<hr>";
-
